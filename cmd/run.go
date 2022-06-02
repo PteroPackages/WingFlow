39e6ad4696f1642c04c78a888c7097624b44c5f8
devnote-dev <inferno0745@gmail.com>
2022-06-03 00:49:43 +0100

    feat(cmd): use ignore mod & fix file resovle

--- cmd/run.go
+++ cmd/run.go
@@ -1,15 +1,16 @@
 package cmd
 
 import (
+	"fmt"
 	"io/fs"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"regexp"
 	"strings"
 
 	"github.com/pteropackages/wingflow/config"
 	"github.com/pteropackages/wingflow/logger"
+	ignore "github.com/sabhiram/go-gitignore"
 	"github.com/spf13/cobra"
 )
 
@@ -31,6 +32,7 @@ func walkAll(root string) ([]string, error) {
 		}
 
 		if strings.HasSuffix(root, entry.Name()) {
+			files = append(files, path)
 			return nil
 		}
 
@@ -78,69 +80,37 @@ func handleRunCmd(cmd *cobra.Command, args []string) {
 		log.Fatal("failed to clone repository into temp directory")
 	}
 
-	// safety check
-	if !contains(cfg.Repository.Exclude, ".git") {
-		cfg.Repository.Exclude = append(cfg.Repository.Exclude, ".git")
-	}
-
 	files, err := filepath.Glob(filepath.Join(temp, "*"))
 	if err != nil {
 		log.WithFatal(err)
 	}
 
-	var includes []*regexp.Regexp
-	replacer := strings.NewReplacer("*", ".*")
-	for _, p := range cfg.Repository.Include {
-		cleaned := replacer.Replace(filepath.Clean(p))
-		includes = append(includes, regexp.MustCompile(cleaned))
+	// safety check
+	if !contains(cfg.Repository.Exclude, ".git") {
+		cfg.Repository.Exclude = append(cfg.Repository.Exclude, ".git")
 	}
-
-	var excludes []*regexp.Regexp
-	for _, p := range cfg.Repository.Exclude {
-		cleaned := replacer.Replace(filepath.Clean(p))
-		excludes = append(excludes, regexp.MustCompile(cleaned))
-	}
-
-	match := func(f string) bool {
-		for _, e := range excludes {
-			if e.Match([]byte(f)) {
-				return false
-			}
-		}
-
-		for _, e := range includes {
-			if e.Match([]byte(f)) {
-				return true
-			}
-		}
-
-		return false
-	}
-
-	var matched []string
-	for _, f := range files {
-		if match(f) {
-			matched = append(matched, f)
-		}
-	}
-
-	if len(matched) == 0 {
-		log.Error("no files matched the configured patterns")
-		log.Fatal("please ensure the patterns resolve to files in the repository")
-	}
+	ignored := ignore.CompileIgnoreLines(cfg.Repository.Exclude...)
 
 	var parsed []string
-	for _, m := range matched {
-		paths, err := walkAll(m)
+	for _, f := range files {
+		paths, err := walkAll(f)
 		if err != nil {
 			log.Debug(err.Error())
 			continue
 		}
 
-		parsed = append(parsed, paths...)
+		for _, p := range paths {
+			if ignored.MatchesPath(p) {
+				continue
+			}
+
+			parsed = append(parsed, p)
+		}
 	}
 
 	if len(parsed) == 0 {
 		log.Fatal("no files could be resolved to an absolute path")
 	}
+
+	fmt.Printf("%v\n", parsed)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/pteropackages/wingflow
 go 1.17
 
 require (
+	github.com/sabhiram/go-gitignore v0.0.0-20210923224102-525f6e181f06
 	github.com/spf13/cobra v1.4.0
 	gopkg.in/yaml.v3 v3.0.1
 )
