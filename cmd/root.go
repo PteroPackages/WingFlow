c0b653a4c08e484ed71047408291f95fabe171a9
devnote-dev <inferno0745@gmail.com>
2023-01-21 23:53:09 +0000

    refactor(config): reimpl config & remove command funcs

--- cmd/root.go
+++ cmd/root.go
@@ -10,26 +10,20 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:       "wflow",
-	Example:   "wflow [flags...] <command>",
-	Short:     "automatic project deployment for pterodactyl",
-	Long:      "a cli tool for automatically deploying projects to pterodactyl",
-	ValidArgs: []string{"init", "check", "run"},
-	Version:   Version,
+	Use:     "wflow",
+	Example: "wflow [flags...] <command>",
+	Short:   "automatic project deployment for pterodactyl",
+	Long:    "A tool for automatically deploying projects to Pterodactyl.",
+	Version: Version,
 }
 
 var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "creates a new config file",
-	Long:  "creates a new config file in the current workspace (or a specified one)",
-	Run: func(cmd *cobra.Command, _ []string) {
-		nc, _ := cmd.Flags().GetBool("no-color")
-		force, _ := cmd.Flags().GetBool("force")
-		dir := cmd.Flag("dir").Value.String()
-		log := logger.New(nc, false)
-
-		err := config.Create(dir, force)
-		if err != nil {
+	Long:  "Creates a new config file in the current workspace.",
+	Run: func(*cobra.Command, []string) {
+		log := logger.New(false, false)
+		if err := config.Create(false); err != nil {
 			log.WithFatal(err)
 		}
 	},
@@ -38,36 +32,36 @@ var initCmd = &cobra.Command{
 var checkCmd = &cobra.Command{
 	Use:   "check",
 	Short: "runs validation checks on the config file",
-	Long:  "runs validation checks on the config file",
-	Run:   handleCheckCmd,
+	Long:  "Runs validation checks on the config file.",
+	Run:   func(*cobra.Command, []string) {},
 }
 
 var runCmd = &cobra.Command{
 	Use:   "run",
 	Short: "fetches and deploys to pterodactyl",
-	Long:  "fetches and deploys the configured project to the pterodactyl server",
-	Run:   handleRunCmd,
+	Long:  "Fetches and deploys the configured project to the pterodactyl server.",
+	Run:   func(*cobra.Command, []string) {},
 }
 
 func init() {
-	dir, _ := os.Getwd()
-	noColor := false
+	// dir, _ := os.Getwd()
+	// noColor := false
 
-	if v, ok := os.LookupEnv("TERM"); ok {
-		noColor = v == "dumb"
-	}
-	if _, ok := os.LookupEnv("NO_COLOR"); ok {
-		noColor = false
-	}
+	// if v, ok := os.LookupEnv("TERM"); ok {
+	// 	noColor = v == "dumb"
+	// }
+	// if _, ok := os.LookupEnv("NO_COLOR"); ok {
+	// 	noColor = false
+	// }
 
-	initCmd.Flags().String("dir", dir, "the directory to create the config in")
-	initCmd.Flags().BoolP("force", "f", false, "force overwrite the config file")
-	initCmd.Flags().Bool("no-color", noColor, "disable color for the output")
-	checkCmd.Flags().String("dir", dir, "the directory of the config file")
-	checkCmd.Flags().Bool("no-color", noColor, "disable color for the output")
-	runCmd.Flags().Bool("debug", false, "output debug logs")
-	runCmd.Flags().String("dir", dir, "the directory of the config file")
-	runCmd.Flags().Bool("no-color", noColor, "disable color for the output")
+	// initCmd.Flags().String("dir", dir, "the directory to create the config in")
+	// initCmd.Flags().BoolP("force", "f", false, "force overwrite the config file")
+	// initCmd.Flags().Bool("no-color", noColor, "disable color for the output")
+	// checkCmd.Flags().String("dir", dir, "the directory of the config file")
+	// checkCmd.Flags().Bool("no-color", noColor, "disable color for the output")
+	// runCmd.Flags().Bool("debug", false, "output debug logs")
+	// runCmd.Flags().String("dir", dir, "the directory of the config file")
+	// runCmd.Flags().Bool("no-color", noColor, "disable color for the output")
 
 	rootCmd.AddCommand(initCmd)
 	rootCmd.AddCommand(checkCmd)
--- config/config.go
+++ config/config.go
@@ -5,55 +5,30 @@ import (
 	"os"
 	"path/filepath"
 
+	"github.com/go-playground/validator/v10"
 	"gopkg.in/yaml.v3"
 )
 
-// Represents the Wingflow config.
 type Config struct {
-	// Git repository credentials
-	// This currently only uses the git address, meaning there is no support for fetching from private repositories yet.
 	Git struct {
-		Address string `validate:"required,url" yaml:"address"`
+		Address string `yaml:"address"`
 		Token   string `yaml:"token,omitempty"`
-	} `validate:"required" yaml:"git"`
+		Files   struct {
+			Include []string `yaml:"include"`
+			Exclude []string `yaml:"exclude"`
+		} `yaml:"files"`
+	} `yaml:"git"`
 
-	// Pterodactyl panel credentials
-	// URL - The url to your panel. Only put the HTTP scheme and your domain
-	//		 (e.g. "https://pterodactyl.domain").
-	//
-	// Key - This should be a CLIENT API key, not an application API key. To prevent
-	//		 request issues, do not restrict this key to an IP address.
-	//
-	// ID  - The identifier of the server to upload and deploy to. You can find this in
-	//		 the URL when going to your server (e.g. "https://pterodacty.domain/servers/84ac52b",
-	//		 the last part is the identifier).
 	Panel struct {
-		URL string `validate:"required,url" yaml:"url"`
-		Key string `validate:"required" yaml:"key"`
-		ID  string `validate:"required" yaml:"id"`
-	} `validate:"required" yaml:"panel"`
-
-	// Configurations for the repository.
-	// Include - A list of file paths or patterns to include in the file upload.
-	// Exclude - A list of file paths or patterns to exclude from being uploaded.
-	Repository struct {
-		Include []string `validate:"required" yaml:"include"`
-		Exclude []string `yaml:"exlude"`
-	} `validate:"required" yaml:"repository"`
-
-	// Configurations for the file system in the server.
-	// ForceKill - Whether to kill the server instead of stopping it.
-	// Ignore    - A list of files to ignore from being deleted.
-	System struct {
-		ForceKill bool     `yaml:"force_kill"`
-		Ignore    []string `yaml:"ignore"`
-	} `validate:"required" yaml:"system"`
-
-	// Commands to execute before starting the main process.
-	PreRun []string `yaml:"pre_run"`
-
-	// Commands to execute after the main process is completed.
-	PostRun []string `yaml:"post_run"`
+		URL         string   `yaml:"url"`
+		Key         string   `yaml:"key"`
+		ID          string   `yaml:"id"`
+		IgnoreFiles []string `yaml:"ignore_files,omitempty"`
+		Signal      struct {
+			Kill    bool    `yaml:"kill"`
+			Timeout float64 `yaml:"timeout,omitempty"`
+		} `yaml:"signal"`
+	} `yaml:"panel"`
 }
 
 func exists(path string) bool {
@@ -64,8 +39,9 @@ func exists(path string) bool {
 	return true
 }
 
-func Fetch(dir string) (*Config, error) {
-	path := filepath.Join(filepath.Clean(dir), "wingflow.yml")
+func Get(check bool) (*Config, error) {
+	cwd, _ := os.Getwd()
+	path := filepath.Join(cwd, ".wflow")
 	if !exists(path) {
 		return nil, errors.New("file path does not exist")
 	}
@@ -76,29 +52,36 @@ func Fetch(dir string) (*Config, error) {
 	}
 
 	var cfg *Config
-	err = yaml.Unmarshal(buf, &cfg)
-	if err != nil {
+	if err = yaml.Unmarshal(buf, &cfg); err != nil {
 		return nil, err
+	}
+
+	if check {
+		if err = validator.New().Struct(cfg); err != nil {
+			return nil, err
+		}
 	}
 
 	return cfg, nil
 }
 
-func Create(dir string, force bool) error {
-	path := filepath.Join(filepath.Clean(dir), "wingflow.yml")
+func Create(force bool) error {
+	cwd, _ := os.Getwd()
+	path := filepath.Join(cwd, ".wflow")
 	if exists(path) && !force {
-		return errors.New("config file already exists at this path")
+		return errors.New("config file already exists in this directory")
 	}
 
-	file, err := os.Create(path)
-	// shouldn't happen, but just in case
+	fd, err := os.Create(path)
 	if err != nil {
 		return err
 	}
+	defer fd.Close()
 
-	buf, _ := yaml.Marshal(Config{})
-	defer file.Close()
-	file.Write(buf)
+	enc := yaml.NewEncoder(fd)
+	enc.SetIndent(2)
+	enc.Encode(Config{})
+	enc.Close()
 
 	return nil
 }
